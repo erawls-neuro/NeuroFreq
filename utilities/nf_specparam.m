7069b3017f193c0e1f0ed91ab49161b703e24e91
erawls-neuro <erawls89@gmail.com>
2026-01-02 18:35:33 -0500

    remove license

--- utilities/nf_specparam.m
+++ utilities/nf_specparam.m
@@ -39,24 +39,7 @@ function parm_spec = nf_specparam( spec, freqs, varargin )
 % f - vector of frequencies in spec
 % various keyword-arg pairs: specparam settings detailed below
 % 
-% -----
-% E. Rawls, erawls89@gmail.com, rawls017@umn.edu. 
-% July 2023
-% Copyright (c) 2023 by E. Rawls.
-% 
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2 of the License, or
-% (at your option) any later version.
 %
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 
 %make input parser
--- utilities/nf_tftransform.m
+++ utilities/nf_tftransform.m
@@ -68,29 +68,7 @@ function [tfRes,p] = nf_tftransform(EEG,varargin)
 %   ridrihaczek: 'cwkernel': Choi-Williams kernel size
 %
 %
-% E. Rawls, erawls89@gmail.com, rawls017@umn.edu. 
-% July 2023
-% Copyright (c) 2023 by E. Rawls.
-% 
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 3 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-%
-%
-% ===========
-% Change Log:
-% ===========
-% 5/23/24: ER removed option "plot"
+
 
 
 % parse input
